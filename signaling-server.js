ff937d27c625b43def04fde44ee1a6f6431e0a9d
苔コッコー <158864238+Kokecoco@users.noreply.github.com>
2024-09-27 20:14:43 +0900

    Update signaling-server.js

--- signaling-server.js
+++ signaling-server.js
@@ -5,13 +5,14 @@ const wss = new WebSocket.Server({ port: port });
 
 let clientCount = 0;   // クライアントの接続番号を管理
 let clients = new Set(); // クライアントを管理
+let firstBuzzerTimestamp = null; // 最初に押されたタイムスタンプを保存
 
 // WebSocketサーバーへの接続イベント
 wss.on('connection', function connection(ws) {
     clientCount++;
     clients.add(ws);
     ws.clientId = clientCount;
-    
+
     console.log(`Client ${ws.clientId} connected.`);
     
     // クライアントに接続された番号を送信
@@ -20,15 +21,48 @@ wss.on('connection', function connection(ws) {
     // メッセージ受信イベント
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
-        
-        // JSONデータに変換して他のすべてのクライアントにメッセージをブロードキャスト
+
+        // JSONデータに変換してメッセージを処理
         const data = JSON.parse(message);
-        
-        clients.forEach(function each(client) {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(JSON.stringify(data)); // JSONに変換して送信
+
+        // タイプが "buzzer" のときだけタイムスタンプの比較を行う
+        if (data.type === 'buzzer') {
+            const currentTimestamp = data.timestamp;
+
+            // 最初の押下タイミングかどうかを判定
+            if (firstBuzzerTimestamp === null || currentTimestamp < firstBuzzerTimestamp) {
+                firstBuzzerTimestamp = currentTimestamp;
+
+                // 最初に押したクライアントを他の全クライアントにブロードキャスト
+                clients.forEach(function each(client) {
+                    if (client.readyState === WebSocket.OPEN) {
+                        client.send(JSON.stringify(data)); // JSONに変換して送信
+                    }
+                });
+            } else {
+                console.log(`Client ${ws.clientId}'s buzzer was ignored due to a later timestamp.`);
             }
-        });
+        } else if (data.type === 'reset') {
+            // クイズのリセット時にタイムスタンプをリセット
+            firstBuzzerTimestamp = null;
+
+            // リセットメッセージをすべてのクライアントに送信
+            clients.forEach(function each(client) {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify(data)); // JSONに変換して送信
+                }
+            });
+        } else if (data.type === 'clientId') {
+            // クライアントIDの処理は既に行っているので無視
+            console.log(`Client ${ws.clientId} received its ID.`);
+        } else {
+            // buzzer, reset, clientId のどれでもない場合はブロードキャスト
+            clients.forEach(function each(client) {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify(data)); // 他のクライアントにそのまま送信
+                }
+            });
+        }
     });
 
     // クライアント切断時のイベント
